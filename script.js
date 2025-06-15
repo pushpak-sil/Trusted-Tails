e916175c9694c54c67721a4f54c449b9d28f552e
Pushpak Sil <pushpaksil.bt24prod@pec.edu.in>
2025-06-16 02:09:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-window.addEventListener('scroll', () => {
-  const sections = document.querySelectorAll('.slide-up');
-  sections.forEach(section => {
-    const top = section.getBoundingClientRect().top;
-    if (top < window.innerHeight * 0.8) {
-      section.style.animationPlayState = 'running';
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.utils.toArray(".slide-up").forEach(section => {
+  gsap.from(section, {
+    y: 100,
+    opacity: 0,
+    duration: 1,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: section,
+      start: "top 80%",
+      toggleActions: "play none none none"
     }
   });
 });
