aaad1388e475e735b59f0b959005c34931accfdb
Pushpak Sil <pushpaksil.bt24prod@pec.edu.in>
2025-06-16 13:21:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+// Initialize GSAP animations with ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+// Animate elements with the "slide-up" class
+gsap.utils.toArray(".slide-up").forEach(el => {
+  gsap.fromTo(el,
+    { y: 100, opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 1.2,
+      ease: "power3.out",
+      scrollTrigger: {
+        trigger: el,
+        start: "top 85%",
+        scrub: true, 
+        once: false,
+        markers: false // set to true if you want to debug
+      }
+    }
+  );
+});
